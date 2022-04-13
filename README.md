8a57c2eecc959c986fc7bfb131bc328f1e83bf1c
Thomas Lafaille <thomas.lafaille@gmail.com>
2022-04-13 13:55:57 +0200

    update

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Retrieve the APR and APY (for daily compounding) for a Cosmos-based blockchain t
 The token name must be updated (default is 'udaric'), and the API server must be enabled on the node (port 1317)
 
 At this time it does not take into account the number of blocks per year, which seems to affect the APR. Will implement that later.
+
+
+<b>UPDATE</b>
+ 
+- Modified the algorithm for taking into account the community tax (however, on Big Dipper it seems to not be the case. If set manually to 0 in the code,<br>
+the APR matches exactly the one that is displayed. But with the tax that is retrieved (0.2 /20%), it's, well, 20% lower).
+- Same applies with the validator commission: to match the displayed value it should be set to 0.
+- Tried to apply the calculation to get the ratio between the expected annual provision vs the actual provision (in terms of number of blocks per year), <br>
+but either the math is wrong, or the actual block number is too far away from the expected one and it makes the APR drop.
--- apr_apy.py
+++ apr_apy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from datetime import datetime
 from threading import Thread
 from time import sleep
 from requests import get
@@ -16,7 +16,10 @@ class GetAprApy(Thread):
         self.inflation_url = "/cosmos/mint/v1beta1/inflation"
         self.bonded_token_url = "/cosmos/staking/v1beta1/pool"
         self.supply_url = "/cosmos/bank/v1beta1/supply"
+        self.params_url = "/cosmos/distribution/v1beta1/params"
+        self.blocks_url = "/cosmos/base/tendermint/v1beta1/blocks/"
         self.token = 'udaric' #change that to the appropriate token name
+        self.commission = 0.05 # the validator commission, let's say 5%.
 
         self.apr = 0
         self.apy = 0
@@ -26,7 +29,7 @@ class GetAprApy(Thread):
 
         @self.app.get("/APR")
         def return_apr():
-            return round(self.apr*100, 3)
+            return round(self.apr,3) #*100, 3)
 
         @self.app.get("/APY")
         def return_apy():
@@ -35,7 +38,7 @@ class GetAprApy(Thread):
     def run(self):
 
         while True:
-            #we need to retrieve the metrics (supply, inflation, and number of bonded tokens) to calculate the APY & APY.
+            #we need to retrieve the metrics (supply, inflation, number of bonded tokens and more) to calculate the APR & APY.
 
             # My deepest apologies for probably the worst piece of code ever written.
             try:
@@ -47,10 +50,22 @@ class GetAprApy(Thread):
 
                 bonded_tokens = get(self.base_url+self.bonded_token_url).text
                 bonded_tokens = int(findall('(?<="bonded_tokens": ")\d+', bonded_tokens)[0])
+
+                tax = get(self.base_url+self.params_url).json()
+                tax = float(tax['params']['community_tax'])
                 #awful.
 
+                #the theoretical APR
+                nominal_apr = self.nominal_APR(supply,inflation,bonded_tokens,tax)
+                #theoretical number of blocks/year
+                theoretical_provision = self.theoretical_provision(supply,inflation)
+                #let's estimate the actual provision
+                actual_provision = self.actual_provision()
+
+                #we have enough data to calculate the actual APR.
+
                 #onto the calculation itself.
-                self.apr = self.APR(supply,inflation,bonded_tokens)
+                self.apr = self.actual_APR(nominal_apr,theoretical_provision,actual_provision)
                 self.apy = self.APY(self.apr)
 
             except Exception as e:
@@ -58,13 +73,43 @@ class GetAprApy(Thread):
 
             sleep(86400) #sleep 24h, yes. Idiotic I know.
 
-    def APR(self,supply,inflation,bonded_tokens):
+    def theoretical_provision(self,supply,inflation):
+        return int(str(supply * (inflation / 100))[:6])
+        # It returns a long float and we need to take only its first 6 digits.
+        # So convert to string, extract the first 6 chars, then back to int. Not sure it can be done more elegantly.
 
-        return ((0.01*supply*inflation)/bonded_tokens) #we'll need to multiply that by 100 when returning the value
+    def actual_provision(self):
+        #check the average block time over the last 1000 blocks and derive the yearly number of blocks.
+        latest_block = get(self.base_url + self.blocks_url + 'latest').json()
+        latest_block_height = int(latest_block['block']['header']['height'])
+        latest_block_time = datetime.strptime(latest_block['block']['header']['time'][:-4:] + 'Z',
+                                              '%Y-%m-%dT%H:%M:%S.%fZ')
+        # now, the timestamp of the earlier block (latest-1000)
+        previous_block_1000_time = datetime.strptime(
+            get(self.base_url + self.blocks_url + str(latest_block_height - 1000)).json()
+            ['block']['header']['time'][:-4:] + 'Z', '%Y-%m-%dT%H:%M:%S.%fZ')
+
+        average_block_time = (latest_block_time.timestamp() - previous_block_1000_time.timestamp()) / 1000
+
+        # now, there are 31,536,000 seconds in a year --365 days, not taking into account leap years because come on.
+        # this allows to get the exact (although somewhat approximate...) provision.
+
+        return 31_536_000/average_block_time
+
+    def nominal_APR(self,supply,inflation,bonded_tokens,tax):
+
+        return (inflation * (1 - tax)) / (bonded_tokens / supply)
+
+    def actual_APR(self,nominal_apr,theoretical_provision,actual_provision):
+
+        #return (nominal_apr*(actual_provision/theoretical_provision))*(1-self.commission)
+        #The above version is not working. Actual and theoretical provisions are significantly different, and affect the result too much.
+        #There must be an error in the upstream formula.
+        return nominal_apr * (1 - self.commission)
 
     def APY(self, apr):
-
-        return round((1 + (apr/365))**365 - 1, 3) #with a daily compounding
+        #need to divide the APR by 100 here.
+        return round(((1 + (apr/(100*365)))**365 - 1)*100, 3) #with a daily compounding
 
 getAprApy = GetAprApy()
 app = getAprApy.app
