61e88ab4a48a1a2281e661b8ff6ee2e11f38607d
Thomas Lafaille <thomas.lafaille@gmail.com>
2022-04-17 12:56:08 +0200

    multiple changes

--- README.md
+++ README.md
@@ -2,15 +2,12 @@
 
 Retrieve the APR and APY (for daily compounding) for a Cosmos-based blockchain through an API (default is port 5006)
 
-The token name must be updated (default is 'udaric'), and the API server must be enabled on the node (port 1317)
+The node API server must be enabled on the node (port 1317 by default).
 
-At this time it does not take into account the number of blocks per year, which seems to affect the APR. Will implement that later.
+Usage: <code>python3 apr_apy -i validator1 port1 token1 commission1 -i validator2 port2 token2 commission2 etc.<code>
 
+Note: the token should be the 'utoken', e.g. urowan or uxprt or whatever.
 
-<b>UPDATE</b>
- 
-- Modified the algorithm for taking into account the community tax (however, on Big Dipper it seems to not be the case. If set manually to 0 in the code,<br>
-the APR matches exactly the one that is displayed. But with the tax that is retrieved (0.2 /20%), it's, well, 20% lower).
-- Same applies with the validator commission: to match the displayed value it should be set to 0.
-- Tried to apply the calculation to get the ratio between the expected annual provision vs the actual provision (in terms of number of blocks per year), <br>
-but either the math is wrong, or the actual block number is too far away from the expected one and it makes the APR drop.
+The program serves an API at localhost:5006/{validator_name}. Values are updated every 24h.
+
+The calculated APR takes into account the actual number of blocks per year (estimated based on the average time of the latest 10k blocks).
--- apr_apy.py
+++ apr_apy.py
@@ -6,34 +6,49 @@ from requests import get
 import uvicorn
 from fastapi import FastAPI
 from re import findall
-
+from argparse import ArgumentParser
+from psutil import process_iter
 
 class GetAprApy(Thread):
-    def __init__(self):
+    def __init__(self, data, app=None):
         super().__init__()
 
-        self.base_url = "http://localhost:1317"
+        try:
+            self.VALIDATOR = data[0]
+            self.PORT = data[1]
+            self.TOKEN = data[2]
+            self.COMMISSION = float(data[3])
+        except:
+            print("\nGarbage was supplied. Please run with for example (with a 5% commission):\n\npython3 apr_apy.py -i desmos 1317 udsm 0.05\n")
+            for proc in process_iter():
+                if "apr_apy.py" in proc.cmdline():
+                    proc.kill()
+#            exit(1)
+
+        self.base_url = f"http://localhost:{self.PORT}" #default is 1317 for the node API
         self.inflation_url = "/cosmos/mint/v1beta1/inflation"
         self.bonded_token_url = "/cosmos/staking/v1beta1/pool"
         self.supply_url = "/cosmos/bank/v1beta1/supply"
-        self.params_url = "/cosmos/distribution/v1beta1/params"
+        self.distribution_params_url = "/cosmos/distribution/v1beta1/params"
+        self.mint_params_url = "/cosmos/mint/v1beta1/params"
         self.blocks_url = "/cosmos/base/tendermint/v1beta1/blocks/"
-        self.token = 'udaric' #change that to the appropriate token name
-        self.commission = 0.05 # the validator commission, let's say 5%.
+        # self.token = 'udaric' #change that to the appropriate token name
+        # self.commission = 0.05 # the validator commission, let's say 5%.
 
         self.apr = 0
         self.apy = 0
 
-        self.app = FastAPI()
-        self.app.type = "00"
+        ###setup FastAPI
+        if app is None:
+            self.app = FastAPI()
+            self.app.type = "00"
+        else:
+            self.app = app
 
-        @self.app.get("/APR")
-        def return_apr():
-            return round(self.apr,3) #*100, 3)
+        @self.app.get(f"/{self.VALIDATOR}")
+        def return_apr_apy():
+            return {'apr': round(self.apr,3), 'apy': self.apy}
 
-        @self.app.get("/APY")
-        def return_apy():
-            return self.apy
 
     def run(self):
 
@@ -43,7 +58,7 @@ class GetAprApy(Thread):
             # My deepest apologies for probably the worst piece of code ever written.
             try:
                 supply = get(self.base_url+self.supply_url).json()
-                supply = int(findall('\d+', str([i for i in supply['supply'] if self.token in i['denom']][0]))[0])
+                supply = int(findall('\d+', str([i for i in supply['supply'] if self.TOKEN in i['denom']][0]))[0])
 
                 inflation = get(self.base_url+self.inflation_url).text
                 inflation = float(findall('\d+.\d+', inflation)[0])*100
@@ -51,14 +66,14 @@ class GetAprApy(Thread):
                 bonded_tokens = get(self.base_url+self.bonded_token_url).text
                 bonded_tokens = int(findall('(?<="bonded_tokens": ")\d+', bonded_tokens)[0])
 
-                tax = get(self.base_url+self.params_url).json()
+                tax = get(self.base_url+self.distribution_params_url).json()
                 tax = float(tax['params']['community_tax'])
                 #awful.
 
                 #the theoretical APR
                 nominal_apr = self.nominal_APR(supply,inflation,bonded_tokens,tax)
                 #theoretical number of blocks/year
-                theoretical_provision = self.theoretical_provision(supply,inflation)
+                theoretical_provision = self.theoretical_provision()
                 #let's estimate the actual provision
                 actual_provision = self.actual_provision()
 
@@ -73,23 +88,22 @@ class GetAprApy(Thread):
 
             sleep(86400) #sleep 24h, yes. Idiotic I know.
 
-    def theoretical_provision(self,supply,inflation):
-        return int(str(supply * (inflation / 100))[:6])
-        # It returns a long float and we need to take only its first 6 digits.
-        # So convert to string, extract the first 6 chars, then back to int. Not sure it can be done more elegantly.
+    def theoretical_provision(self):
+        provision = get(self.base_url+self.mint_params_url).json()
+        return int(provision['params']['blocks_per_year'])
 
     def actual_provision(self):
-        #check the average block time over the last 1000 blocks and derive the yearly number of blocks.
+        #check the average block time over the last 10k blocks and derive the yearly number of blocks.
         latest_block = get(self.base_url + self.blocks_url + 'latest').json()
         latest_block_height = int(latest_block['block']['header']['height'])
         latest_block_time = datetime.strptime(latest_block['block']['header']['time'][:-4:] + 'Z',
                                               '%Y-%m-%dT%H:%M:%S.%fZ')
         # now, the timestamp of the earlier block (latest-1000)
         previous_block_1000_time = datetime.strptime(
-            get(self.base_url + self.blocks_url + str(latest_block_height - 1000)).json()
+            get(self.base_url + self.blocks_url + str(latest_block_height - 10000)).json()
             ['block']['header']['time'][:-4:] + 'Z', '%Y-%m-%dT%H:%M:%S.%fZ')
 
-        average_block_time = (latest_block_time.timestamp() - previous_block_1000_time.timestamp()) / 1000
+        average_block_time = (latest_block_time.timestamp() - previous_block_1000_time.timestamp()) / 10000
 
         # now, there are 31,536,000 seconds in a year --365 days, not taking into account leap years because come on.
         # this allows to get the exact (although somewhat approximate...) provision.
@@ -102,19 +116,29 @@ class GetAprApy(Thread):
 
     def actual_APR(self,nominal_apr,theoretical_provision,actual_provision):
 
-        #return (nominal_apr*(actual_provision/theoretical_provision))*(1-self.commission)
-        #The above version is not working. Actual and theoretical provisions are significantly different, and affect the result too much.
-        #There must be an error in the upstream formula.
-        return nominal_apr * (1 - self.commission)
+        # print("actual_APR", nominal_apr*(actual_provision/theoretical_provision)*(1-self.COMMISSION))
+        # print("theorical_APR", nominal_apr * (1 - self.COMMISSION))
+        #return the adjusted apr based on the actual number of blocks per year.
+        return (nominal_apr*(actual_provision/theoretical_provision))*(1-self.COMMISSION)
+
 
     def APY(self, apr):
         #need to divide the APR by 100 here.
         return round(((1 + (apr/(100*365)))**365 - 1)*100, 3) #with a daily compounding
 
-getAprApy = GetAprApy()
-app = getAprApy.app
-getAprApy.daemon = True
-getAprApy.start()
+
+parser = ArgumentParser()
+parser.add_argument('-i', nargs='+', action='append', help='Usage: python3 apr_apy.py -i validator1 port1 token1 commission1 -i validator2 port2 token2 commission2 etc.')
+args = parser.parse_args()
+
+app = None
+
+for i in args.i:
+    getAprApy = GetAprApy(i, app)
+    getAprApy.daemon = True
+    if app is None:
+        app = getAprApy.app
+    getAprApy.start()
 
 
 uvicorn.run(app, host='0.0.0.0', port=5006)
